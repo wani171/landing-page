b95701dd3967bf0e8b2971077a81863cd055e7c1
wani171 <dhanashri1907@gmail.com>
2023-01-11 21:44:00 +0530

    updated contact page

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,9 +1,42 @@
 import React from 'react'
 
-const Contact = () => {
+function Contact() {
+  const [result, setResult] = React.useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "af25645b-e00c-4205-9c65-d694e704b2d8");
+
+    const res = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData
+    }).then((res) => res.json());
+
+    if (res.success) {
+      console.log("Success", res);
+      setResult(res.message);
+    } else {
+      console.log("Error", res);
+      setResult(res.message);
+    }
+  };
+
   return (
-    <div>Contact</div>
-  )
+    <div className="App">
+      <form onSubmit={onSubmit}>
+        <input type="text" name="name"/>
+        <input type="email" name="email"/>
+        <textarea name="message"></textarea>
+        <input type="submit" />
+      </form>
+      <span>{result}</span>
+    </div>
+  
+    
+  );
 }
 
 export default Contact
