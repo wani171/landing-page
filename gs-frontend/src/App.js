4544c73897b9c042fec2c0e0cfb67d051fccca64
wani171 <dhanashri1907@gmail.com>
2023-01-15 19:28:45 +0530

    added hashrouter

--- gs-frontend/src/App.js
+++ gs-frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {HashRouter as Router, Routes, Route } from "react-router-dom";
 import About from "./About";
 import Home from "./Home";
 import Products from "./Products";
